e5fd937ca83c1e8611f9e91a301c840c0c12904f
stevopablo <stevaopabllo@gmai.com>
2025-02-04 21:36:08 -0300

    adicionar function

--- src/Controller/Inventario.js
+++ src/Controller/Inventario.js
@@ -6,18 +6,33 @@ async function pegarTodosItens(req, res) {
         res.status(200).json(itens)
     }catch(err){
         console.log("🚀 ~ pegarTodosItens ~ err:", err)
+        res.status(500).send(err.message);
     }
 }
 
 async function adicionarItem(req, res) {
-    const { nome, quantidade, descricao, imagem } = req.body
-    try{
-        
-    }catch(err){
-        console.log("🚀 ~ adicionarItem ~ err:", err)
+    const { nome, quantidade, descricao, preco, nota } = req.body;
+    try {
+        const inventario = new Inventario({
+            nome,
+            quantidade,
+            descricao,
+            preco,
+            nota,
+            imagem: {
+                data: req.file.buffer,
+                contentType: req.file.mimetype
+            }
+        });
+        await inventario.save();
+        res.status(201).json(inventario);
+    } catch (err) {
+        console.log("🚀 ~ adicionarItem ~ err:", err);
+        res.status(400).send(err.message);
     }
 }
 
-// module.exports = {
-//     pegarTodosItens
-// }
+module.exports = {
+    pegarTodosItens,
+    adicionarItem
+}
